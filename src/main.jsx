0a00aa4c4bf608a4d4f628f89046eadbc00c6aff
SMNoushan <noushan.md@gmail.com>
2024-03-25 23:27:00 +0600

    added blog, nested route (content, author)

--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Blogs from './pages/Blogs.jsx';
-import Bookmarks from './pages/Bookmarks.jsx';
-import Home from './pages/Home.jsx';
-import MainLayout from './layouts/MainLayout.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/blogs",
-        element: <Blogs />,
-        loader: () => fetch('https://dev.to/api/articles?per_page=20&top=7')
-      },
-      {
-        path: "/bookmarks",
-        element: <Bookmarks />,
-      },
-    ]
-  },
-
-]);
+import { router } from './routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
